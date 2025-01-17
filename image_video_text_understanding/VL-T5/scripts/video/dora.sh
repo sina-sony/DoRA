b368114ff04ea8704afadad6d386cf6d8cba6ce6
Sina Sajadmanesh <sina.sajadmanesh@sony.com>
2025-01-17 10:37:36 +0000

    Add error handling to dora.sh script

--- image_video_text_understanding/VL-T5/scripts/video/dora.sh
+++ image_video_text_understanding/VL-T5/scripts/video/dora.sh
@@ -5,7 +5,7 @@
 # and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
-
+set -e
 task=multitask_video
 
 # or bart
